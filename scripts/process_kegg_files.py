7c3ce9460efe0593d0514b1fa11280e1eea5bd05
Mahmudur <mahmudhera93@gmail.com>
2022-10-05 23:41:01 -0400

    filter single chromosome bacteria

--- scripts/process_kegg_files.py
+++ scripts/process_kegg_files.py
@@ -26,6 +26,32 @@ def read_organism_table(org_table_filename):
 
     return ret_dic
 
+def read_organism_table_for_single_chr_organisms(org_table_filename):
+    # read dataframe
+    df = pd.read_csv(org_table_filename, delimiter='\t')
+
+    # keep only bacteria
+    df['lineage'] = df['lineage'].str.lower()
+    df = df[ df['lineage'].str.contains('bacteria') ]
+
+    # take interesting things
+    gb_ids = df['gb_ncbi_seq_id'].tolist()
+    org_codes = df['org_code'].tolist()
+    names = df['name'].tolist()
+
+    # keep only non-empty ncbi things here
+    ret_dic = {}
+    for gb_id, org_code, name in list( zip(gb_ids, org_codes, names) ):
+        if gb_id == '[]':
+            continue
+        else:
+            gb_id = gb_id[1:-1].replace('\'', '').split(',')
+            if len(gb_id) > 1:
+                continue
+            ret_dic[org_code] = gb_id
+
+    return ret_dic
+
 def read_gene_ids(kegg_gene_filename):
     f = open(kegg_gene_filename, 'r')
     lines = f.readlines()[1:]
@@ -44,7 +70,10 @@ if __name__ == '__main__':
     args = parse_args()
     org_table_filename = args.org_table_file
     org_code_to_ncbi_ids = read_organism_table(org_table_filename)
-    print( org_code_to_ncbi_ids['elc'] )
+    print( len( org_code_to_ncbi_ids.keys() ) )
+
+    org_code_to_ncbi_ids_single_chr = read_organism_table(org_table_filename)
+    print( len( org_code_to_ncbi_ids_single_chr.keys() ) )
 
     sample_gene_filename = 'data/sce_kegg_genes.txt'
     gene_ids = read_gene_ids(sample_gene_filename)
