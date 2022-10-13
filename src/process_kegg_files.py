27511ccc3a80cbf31666d0acd9e0ba6567975cff
Mahmudur <mahmudhera93@gmail.com>
2022-10-12 21:56:38 -0400

    reduced num of genes

--- src/process_kegg_files.py
+++ src/process_kegg_files.py
@@ -275,7 +275,7 @@ def main(): # pragma: no cover
 
         mapping_records = []
         mapping_filename = os.path.join(genome_dir, org_code+'_mapping.csv')
-        for gene_name, ko_id, nt_seq, aa_seq in tqdm(org_code_to_gene_and_ko[org_code]):
+        for gene_name, ko_id, nt_seq, aa_seq in tqdm(org_code_to_gene_and_ko[org_code][:10]):
             start_pos, end_pos, strand = read_gene_start_and_end_positions(gene_name)
             genome_name = org_code
             contig_id = key_in_dict
